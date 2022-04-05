e7de941120ba7081ef157f0f04ccdb75a41a420f
ElenaLickel <elickel.ieu2020@student.ie.edu>
2022-04-05 21:02:42 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from matplotlib import pylab as plt
+import pandas as pd
+
+df_ko = pd.read_csv('KO.csv')
+print(df_ko.head())
+df_pep = pd.read_csv('PEP.csv')
+print(df_pep.head())
+
+index2 = []
+for date2 in df_pep.Date:
+    if df_ko.index[df_ko.Date == date2].values.size:
+        index2.append(int(df_ko.index[df_ko.Date == date2].values[0]))
+print(index2)
+
+mean = df_ko["Close"].mean()
+
+plt.figure('Coca Cola Stock vs Pepsi Stock')
+plt.plot(df_ko['Date'],df_ko['Close'], linewidth=0.8, color = 'r')
+plt.plot(df_pep['Date'],df_pep['Close'])
+
+plt.xlabel('Dates')
+plt.ylabel('Stock Price')
+plt.legend()
+
+plt.show()
+
