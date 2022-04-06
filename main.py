e2842e817caec643ef8ceb623ab070b7e287d78b
ElenaLickel <elickel.ieu2020@student.ie.edu>
2022-04-06 12:21:28 +0200

    adjusted date

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from matplotlib import pylab as plt
 import pandas as pd
 
 df_ko = pd.read_csv('KO.csv')
+df_ko['Date'] = pd.to_datetime(df_ko.Date)
 print(df_ko.head())
 df_pep = pd.read_csv('PEP.csv')
+df_pep['Date'] = pd.to_datetime(df_pep.Date)
 print(df_pep.head())
 
 index2 = []
@@ -20,7 +22,7 @@ plt.plot(df_pep['Date'],df_pep['Close'])
 
 plt.xlabel('Dates')
 plt.ylabel('Stock Price')
-plt.legend()
+
 
 plt.show()
 
